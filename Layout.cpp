143d63fa9051e0a1f729fa0f34bdd76f86ee9621
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-10-08 12:11:25 +0200

    Layout of a Simple C++ Program

---
+++ Layout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 
+  Variable_Declarations
+    
+  Statement_1
+  Statement_2
+    
+    ...
+    
+  Statement_Last
+    
+    
+  return 0;
+  
+}

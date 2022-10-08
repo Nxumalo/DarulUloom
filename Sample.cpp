118290b0e763e21874ee9d5d73a101301ccd2378
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-10-08 11:59:35 +0200

    Create Sample.cpp

---
+++ Sample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  
+  int number_of_pods, peas_per_pod, total_peas;
+  
+  cout << " Press return after entering a number. \n";
+  cout << " Enter the number of pods: \n";
+  cin >> number of pods

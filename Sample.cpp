6434778b359d18348eecd9dad0dbc0daba102cd5
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-10-08 12:07:46 +0200

    A sample of C++ Program
    Peas in a Pod

--- Sample.cpp
+++ Sample.cpp
@@ -6,4 +6,21 @@ int main(){
   
   cout << " Press return after entering a number. \n";
   cout << " Enter the number of pods: \n";
-  cin >> number of pods
+  cin >> number of pods;
+  cout << " Enter the number of peas in a pod: \n";
+  cin >> peas_per_pod;
+  
+  total_peas = number_of_pods * peas_per_pod;
+  
+  cout << " If you have ";
+  cout <<  number_of_pods;
+  cout << " pea pods \n";
+  cout << peas_per_pod;
+  cout << " peas in each pod, then\n";
+  cout << "you have ";
+  cout << total_peas;
+  cout << " peas in all the pods. \n";
+  
+  return 0;
+  
+}

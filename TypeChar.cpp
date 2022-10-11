04009488405cab5d4c6af3b3949926522c4873cb
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-10-11 15:48:44 +0200

    The Type Char

--- TypeChar.cpp
+++ TypeChar.cpp
@@ -5,4 +5,20 @@ int main()
 {
   
   char symboll2, symbol2, symbol3;
+ 
+  cout << "enter two intials, with any periods: \n";
+  cin >> symbol1 >> symbol2;
+  
+  cout << "The two initials are: \n";
+  cout << symbol1 << symbol2 << endl;
+  
+  cout << "Once more with a space: \n";
+  symbol3 = ' ';
+  cout << symbol1 << symbol3 << symbol2 << end1;
+  
+  cout << " That's all. ";
+  
+  return 0;
+  
+}
   

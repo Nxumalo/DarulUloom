1d5894b1b7e8727f9381c878b2f01a7ea636b5c1
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-10-19 16:52:44 +0300

    A while loop

---
+++ Whileloop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int count_down;
+  cout << "How many greatings do you want? ";
+  cin >> count_down;
+  
+  while (count_down > 0);
+  {
+      cout << "Moloweni";
+      cout_down = count_down - 1;
+  }
+  
+  cout << endl;
+  cout << "That's all!\n";
+  
+  return 0;
+}

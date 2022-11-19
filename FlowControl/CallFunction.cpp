de9f3de67523cc4dcd8ee52ee1c66e50d0ea4483
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-11-19 11:50:31 +0200

    A Call Function

---
+++ FlowControl/CallFunction.cpp
@@ -0,0 +1,30 @@
+// Computes the size of a chicken house that can be purchased
+// given the user's budget 
+#include <iostream>
+#include <cmath>
+using namspace std;
+
+int main()
+{
+  const double COST_PER_SQ_FT = 10.50;
+  double budget, area , length_side;
+  
+  cout << " Enter the amount budget for your chicken house $ ";
+  cin >> budget;
+  
+  area = budget/COST_PER_SQ_FT;
+  length_side = sqrt(area);
+  
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(2);
+  
+  cout << " For a price of $ " << budget << endl
+    << " I can build you a luxurious square chicken house\n"
+    << " that is " << length_side
+    << " feet on each side.\n";
+  
+  return 0;
+  
+    
+}

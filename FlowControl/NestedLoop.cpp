15ab7cd2291a307d7f593f5f66fa0d502983eb21
Fraser Nxumalo <3538264@myuwc.ac.za>
2022-11-19 11:02:58 +0200

    Create NestedLoop.cpp

---
+++ FlowControl/NestedLoop.cpp
@@ -0,0 +1,19 @@
+// Determines the total number of green-necked vulture eggs
+// counted by all conservationists in the conservation district.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "This program tallies conservationist reports\n
+      << " on the green-necked vulture.\n"
+      << " Each conservationist's report consists of\n"
+      << " a list of numbers. Each number is the count of\n
+      << " the eggs observed in one"
+      << " green-necked vulture nest. \n"
+      << " This program then tallies "
+      << " the total number of eggs.\n";
+  
+  int number_
+
+}

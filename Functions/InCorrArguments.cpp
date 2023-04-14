c94abb8e3c68a9cadab0a60d9c1ac27541e68b3a
Fraser Nxumalo <3538264@myuwc.ac.za>
2023-04-14 16:05:28 +0200

    Incorrectly  Ordered Arguments

--- Functions/InCorrArguments.cpp
+++ Functions/InCorrArguments.cpp
@@ -15,4 +15,29 @@ int main(){
     << " and the minimum needed to pass:\n ";
   cin >> score >> need_to_pass;
   
+  letter_grade = grade(need_to_pass, score);
+  
+  cout << " You received a score of " << score << endl
+    << " Minimum to pass is " << need_to_pass << endl;
+  
+  if(letter_grade == ' P ')
+    cout << " You Passed. Congratulations! \n ";
+  else
+    cout << " Sorry, You failed. \n ";
+  
+  cout << letter_grade
+       << " will be entered in your record. \n ";
+  
+  return 0;
+   
+}
+
+char grade(int received_par, int min_score_par)
+{
+  if(received_par >= min_score_par)
+    return 'P';
+  else
+    return 'F';
+  
+}
 }

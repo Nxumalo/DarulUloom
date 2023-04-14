fe5389d42e7f2a274769cf6698122a6f170e7478
Fraser Nxumalo <3538264@myuwc.ac.za>
2023-04-14 15:58:42 +0200

    Update InCorrArguments.cpp

--- Functions/InCorrArguments.cpp
+++ Functions/InCorrArguments.cpp
@@ -1,1 +1,18 @@
+// Determines user's grade. Grades are Pass or Fail.
+#include <iostream>
+using namespace std;
 
+char grade(int received_par, int min_score_par);
+// Returns ' P ' for passing, if received_par is
+// min_score_par or higher. Otherwise returns 'F' for failing
+
+int main(){
+  
+ int score, need_to_pass;
+ char letter_grade;
+  
+  cout << "  Enter your score " 
+    << " and the minimum needed to pass:\n ";
+  cin >> score >> need_to_pass;
+  
+}

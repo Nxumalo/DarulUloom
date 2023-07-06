6ffc564c470126b1ee9331ebd2c1f563b04137fd
Fraser Nxumalo <3538264@myuwc.ac.za>
2023-07-06 12:03:12 +0200

    Create VoidFunct.cpp

---
+++ Subtasks/VoidFunct.cpp
@@ -0,0 +1,8 @@
+// Program to covert a Fahrenheit temperature toa Celsius temperature.
+#include <iostream>
+
+void initialize_screen();
+// Separates current output from
+// the output of the previously  run program.
+
+double celsius( double fahrenheight);

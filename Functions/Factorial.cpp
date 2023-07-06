729f9958d9a7668c110fc9560eea249649ab1a8f
Fraser Nxumalo <3538264@myuwc.ac.za>
2023-07-06 09:49:43 +0200

    Factorial Function

---
+++ Functions/Factorial.cpp
@@ -0,0 +1,18 @@
+int factorial(int n);
+// Returns factorial of n.
+// The argument n should be nonnegative.
+
+// Function Definition
+
+int factorial(int n)
+{
+  int product = 1;
+  while(n>0)
+{  
+  product = n * product;
+  n--;
+}
+
+return product;
+
+}

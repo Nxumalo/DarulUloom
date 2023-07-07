18fe3bf92037ed92e19827bb08ebb295282cfaf1
Fraser Nxumalo <3538264@myuwc.ac.za>
2023-07-07 11:05:08 +0200

    Void Fu

--- Subtasks/VoidFunct.cpp
+++ Subtasks/VoidFunct.cpp
@@ -6,3 +6,53 @@ void initialize_screen();
 // the output of the previously  run program.
 
 double celsius( double fahrenheight);
+// Converts a Fahrenheit temperature
+// to a Celsuis temperature
+
+void show_results(double f_degrees, double c_degrees);
+// Displays output. Assumes that c_degrees
+// Celsuis is equivalent to f_degrees Fahrenheit
+
+int main()
+{
+    using namespace std;
+    double f_temperature, c_temperature;
+
+    initialize_screen();
+    cout << " I will convert a Fahrenheit temperature "
+      << "to Celsuis.\n"
+      << " Enter a temperature in Fahrenheit: ";
+    cin >> f_temperature;
+
+    c_temperature = celsius(f_temperature);
+
+    show_results(f_temperature, c_temperature);
+    return 0;
+
+}
+
+// Definition uses iostream
+void intialize_screen()
+{
+    using namespace std;
+    cout << endl;
+    return                             // The return is optional
+}
+
+double celsius(double fahrenheit)
+{
+    return((5.0/9.0)*(fahrenheit - 32));
+}
+
+// Definition uses iostream:
+void show_results(double f_degrees, double c_degrees){
+
+    using namespace std;
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(1);
+    cout << f_degrees
+        << " degrees Fahrenheit is equivalent to\n"
+        << c_degrees << " degrees Celsius.\n";
+    return;                            // The return is optional
+}
